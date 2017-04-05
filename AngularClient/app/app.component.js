04b35330c5316e5ffbaecc5f90baa8bc922c1e6c
orzech <michalorzechowski123@gmail.com>
2017-04-05 23:03:12 +0200

    ***

--- AngularClient/app/app.component.js
+++ AngularClient/app/app.component.js
@@ -12,30 +12,38 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var io = require("socket.io-client");
 var AppComponent = (function () {
-    function AppComponent() {
-        var _this = this;
+    function AppComponent(elRef) {
+        this.elRef = elRef;
         this.self = this;
         this.first = { content: 'Angular 2 Start' };
         this.color = 'green';
         this.xx = 22;
         this.socket = null;
+        this.canvas = null;
+    }
+    AppComponent.prototype.ngAfterViewInit = function () {
+        var _this = this;
         this.socket = io.connect("http://localhost:3000", { reconnection: true });
+        var canvas = this.elRef.nativeElement.querySelector('#canvas1');
         this.socket.on("connect", function (server) {
-            console.log("Connected!");
+            console.log("Connectedddd!");
         });
         this.socket.on("update", function (update) {
             console.log(update);
         });
         setInterval(function () {
             _this.socket.emit("changeDirection", Math.floor((Math.random() * 4) + 1));
+            var ctx = canvas.getContext("2d");
+            ctx.fillStyle = "rgb(" + Math.floor((Math.random() * 255) + 1) + ", 0, 255)";
+            ctx.fillRect(0, 0, 150, 75);
         }, 2000);
-    }
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n    <h2>{{first.content}}</h2>\n    <h2 [innerText]=\"xx\"></h2>\n    <h1 [style.color]=\"color\">{{first.content}}</h1>\n  "
+            template: "\n    <h2>{{first.content}}</h2>\n    <h2 [innerText]=\"xx\"></h2>\n    <h1 [style.color]=\"color\">{{first.content}}</h1>\n    <canvas id=\"canvas1\" width=\"200\" height=\"100\"></canvas>\n  ",
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [core_1.ElementRef])
     ], AppComponent);
     return AppComponent;
 }());
--- AngularClient/app/app.component.ts
+++ AngularClient/app/app.component.ts
@@ -1,39 +1,51 @@
 /// <reference path="../node_modules/@types/socket.io-client/index.d.ts" /> 
 
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import * as io from "socket.io-client";
 
 @Component({
   selector: 'my-app',
- template: `
+  template: `
     <h2>{{first.content}}</h2>
     <h2 [innerText]="xx"></h2>
     <h1 [style.color]="color">{{first.content}}</h1>
-  `
+    <canvas id="canvas1" width="200" height="100"></canvas>
+  `,
+  
 })
-export class AppComponent { 
-    self = this;
-    first = { content: 'Angular 2 Start' };
-    color = 'green';
 
-    xx : number= 22 ;
+export class AppComponent 
+{ 
+  self = this;
+  first = { content: 'Angular 2 Start' };
+  color = 'green';
 
-    socket : SocketIOClient.Socket = null;
+  xx : number= 22 ;
 
-    constructor()
-    {
-      this.socket = io.connect("http://localhost:3000", { reconnection: true });
-      this.socket.on("connect", (server: SocketIOClient.Socket) => {
-            console.log("Connected!");
-        });
+  socket : SocketIOClient.Socket = null;
+  canvas = null;
 
-        this.socket.on("update", (update) => {
-          console.log(update);
-        });
+  constructor(private elRef:ElementRef) {}
+  
+  ngAfterViewInit()
+  {
+    this.socket = io.connect("http://localhost:3000", { reconnection: true });
+    var canvas = <HTMLCanvasElement>this.elRef.nativeElement.querySelector('#canvas1');
 
-        setInterval(() =>{
-            
-            this.socket.emit("changeDirection", Math.floor((Math.random() * 4) + 1));
-        }, 2000);
-    }
+    this.socket.on("connect", (server: SocketIOClient.Socket) => {
+        console.log("Connectedddd!");
+    });
+
+    this.socket.on("update", (update) => {
+      console.log(update);
+    });
+
+    setInterval(() => {
+        this.socket.emit("changeDirection", Math.floor((Math.random() * 4) + 1));
+
+        var ctx = canvas.getContext("2d");
+        ctx.fillStyle = "rgb(" + Math.floor((Math.random() * 255) + 1) + ", 0, 255)";
+        ctx.fillRect(0,0,150,75);
+    }, 2000);
+  }
 }
